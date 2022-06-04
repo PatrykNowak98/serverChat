783664c1635f0820798d8b9009caa1f5f9313fc3
unknown <PatrykNowak98@users.noreply.github.com>
2022-06-04 17:41:50 +0200

    Changes and reworks

--- client.py
+++ client.py
@@ -1,31 +1,63 @@
-import socket
 import threading
+import socket
 
-nick = input("Choose your nickname: ")
+nickname = input("Choose a nickname: ")
 
-clt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clt.connect(("127.0.0.1", 55555))
+if nickname == 'admin':
+	password = input("Enter password for admin controls: ")
 
-def rcv():
-    while True:
-        try:
-            msg = clt.recv(1024).decode("ascii")
-            if msg == "NICK":
-                clt.send(nick.encode("ascii"))
-            else:
-                print(msg)
-        except:
-            print("Error")
-            clt.close()
-            break
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 5555))
 
-def wr():
-    while True:
-        msg = f"{nick}: {input('')}"
-        clt.send(msg.encode("ascii"))
+stop_thread = False
 
-recieve_thread = threading.Thread(target=rcv)
-recieve_thread.start()
 
-write_thread = threading.Thread(target=wr)
+def receive():
+	while True:
+		global stop_thread
+		if stop_thread:
+			break
+		try:
+			message = client.recv(1024).decode('ascii')
+			if message == "NICK":
+				client.send(nickname.encode('ascii'))
+				next_message = client.recv(1024).decode('ascii')
+				if next_message == 'PASS':
+					client.send(password.encode('ascii'))
+					if client.recv(1024).decode('ascii') == 'REFUSE':
+						print("Connection was refused! Wrong password")
+						stop_thread = True
+				elif next_message == 'BAN':
+					print('Connection refused. Reason: Ban.')
+					client.close()
+					stop_thread = True
+			else:
+				print(message)
+		except:
+			print("An error occurred!")
+			client.close()
+			break
+
+
+def write():
+	while True:	
+		if stop_thread:
+			break
+		message = f'{nickname}: {input("")}'
+		if message[len(nickname)+2:].startswith('/'):
+			if nickname == 'admin':
+				if message[len(nickname)+2:].startswith('/kick'):
+					client.send(f'KICK {message[len(nickname)+2+6:]}'.encode('ascii'))
+				if message[len(nickname)+2:].startswith('/ban'):
+					client.send(f'BAN {message[len(nickname)+2+5:]}'.encode('ascii'))
+			else:
+				print("Command can only be executed by the admin!")
+		else:
+			client.send(message.encode('ascii'))
+
+
+receive_thread = threading.Thread(target = receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target = write)
 write_thread.start()
--- server.py
+++ server.py
@@ -1,54 +1,102 @@
 import threading
 import socket
 
-host = "127.0.0.1"
-port = 55555
+host = '127.0.0.1'
+port = 5555
 
-srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srv.bind((host, port))
-srv.listen()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
 
-clts = []
-nicks = []
-
-
-def broadcast(msg):
-    for clt in clts:
-        clt.send(msg)
+clients = []
+nicknames = []
 
 
-def handle(clt):
-    while True:
-        try:
-            msg = clt.recv(1024)
-            broadcast(msg)
-        except:
-            index = clts.index(clt)
-            clts.remove(clt)
-            clt.close()
-            nick = nicks[index]
-            broadcast(f"{nick} has disconnected.".encode("ascii"))
-            nicks.remove(nick)
-            break
+def brodcast(message):
+	for client in clients:
+		client.send(message)
 
 
-def rcv():
-    while True:
-        clt, adrs = srv.accept()
-        print(f"Connected with{str(adrs)}")
+def handle(client):
+	while True:
+		try:
+			msg = message = client.recv(1024)
+			if msg.decode('ascii').startswith('KICK'):
+				if nicknames[clients.index(client)] == 'admin':
+					name_to_kick = msg.decode('ascii')[5:]
+					kick_user(name_to_kick)
+				else:
+					client.send('Command was refused!'.encode('ascii'))
+			elif msg.decode('ascii').startswith('BAN'):
+				if nicknames[clients.index(client)] == 'admin':
+					name_to_ban = msg.decode('ascii')[4:]
+					kick_user(name_to_ban)
+					with open('bans.txt', 'a') as f:
+						f.write(f'{name_to_ban}\n')
+					print(f'{name_to_ban} was banned!')
+				else:
+					client.send('Command was refused!'.encode('ascii'))
+			else:
+				brodcast(message)
+		except:
+			index = clients.index(client)
+			clients.remove(client)
+			client.close()
+			nickname = nicknames[index]
+			brodcast(f'{nickname} left the chat!'.encode('ascii'))
+			nicknames.remove(nickname)
+			break
 
-        clt.send("NICK".encode("ascii"))
-        nick = clt.recv(1024).decode("ascii")
-        nicks.append(nick)
-        clts.append(clt)
 
-        print(f"The clients nicname is {nick}.")
-        broadcast(f"{nick} has connected.".encode("ascii"))
-        clt.send("Sucessfully connected to the server.".encode("ascii"))
+def receive():
+	"""
+	This is the main method for the server.
+	"""
+	while True:
+		client, address = server.accept() 
+		print(f'Connected with {str(address)}')
 
-        thread = threading.Thread(target=handle, args=(clt,))
-        thread.start()
+		client.send('NICK'.encode('ascii'))
+		nickname = client.recv(1024).decode('ascii')
+		
+		with open('bans.txt', 'r') as f:
+			bans = f.readlines()
+
+		if nickname + '\n' in bans:
+			client.send('BAN'.encode('ascii'))
+			client.close()
+			continue
+
+		if nickname == 'admin':
+			client.send('PASS'.encode('ascii'))
+			password = client.recv(1024).decode('ascii')
+
+			if password != 'adminpass':
+				client.send('REFUSE'.encode('ascii'))
+				client.close()
+				continue
+
+		nicknames.append(nickname)
+		clients.append(client)
+
+		print(f'Nickname of the client is {nickname}!')
+		brodcast(f'{nickname} joined the chatroom.'.encode('ascii'))
+		client.send('Connected to the server!'.encode('ascii'))
+
+		thread = threading.Thread(target=handle, args=(client,))
+		thread.start()
+
+
+def kick_user(name):
+	if name in nicknames:
+		name_index = nicknames.index(name)
+		client_to_kick = clients[name_index]
+		clients.remove(client_to_kick)
+		client_to_kick.send('You were kicked by an admin!'.encode('ascii'))
+		client_to_kick.close()
+		nicknames.remove(name)
+		brodcast(f'{name} was kicked by the admin!'.encode('ascii'))
 
 
 print("Server is listening...")
-rcv()
+receive()
